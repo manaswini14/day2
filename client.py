b73c78d83e9cbf4f199c17ade8af11dc3f6ff0d0
manaswini14 <swathi.manaswini@gmail.com>
2014-12-23 11:39:13 +0530

    First Commit

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import json
+import yaml
 import nltk
 json_data=open('all_data.json')
-data = json.load(json_data)
+data = yaml.load(json_data)
 di = {} #key is sentence and value is rel array
 count = 0
 for contrib in data["root"]:
@@ -16,7 +17,7 @@ for contrib in data["root"]:
 				continue
 
 
-
+print di.keys()
 supported_tags_list = ["Price","OS","Org","Phone","Size","Model","Other","Family"]
 supported_rel_tags = ["interest_intent","comparison","price_query","feature_query","irrelevant"]
 
